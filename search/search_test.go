2afcff1b1a3d19296b234c2fba1ff58d5e158a30
Guilherme Puida <guilherme@puida.xyz>
2024-03-10 00:41:28 -0300

    fix gotT and gotQ assignment

--- search/search_test.go
+++ search/search_test.go
@@ -34,7 +34,7 @@ func Test_nextSiec(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotT, gotQ := nextSiec(tt.args.M)
+			gotQ, gotT := nextSiec(tt.args.M)
 			if !reflect.DeepEqual(gotT, tt.wantT) {
 				t.Errorf("nextSiec() gotT = %v, want %v", gotT, tt.wantT)
 			}
